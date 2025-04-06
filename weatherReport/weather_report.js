4f6a3dfa5c6c478693ebba4227fad52411f1b07c
mylonsgitstuff <mylon.gladden@gmail.com>
2025-04-06 14:26:09 -0400

    added lat long functionality

--- weatherReport/weather_report.js
+++ weatherReport/weather_report.js
@@ -14,4 +14,23 @@ function showweatherDetails(event) {
     })
 }
 
+function weatherByLatLong(event) {
+    event.preventDefault();
+    const lat = document.getElementById('lat').value;
+    const long = document.getElementById('long').value;
+    const apiKey2 = '585832d1f5b9b504dfc9a89d8efe3c0e';
+    const apiUrl2 = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${apiKey2}`;
+
+    fetch(apiUrl2)
+    .then(response => response.json())
+    .then(data => {
+      const weatherInfo = document.getElementById('weatherInfo');
+      weatherInfo.innerHTML = `<h2>Weather in ${data.name}</h2>
+                              <p>Temperature: ${data.main.temp} &#8451;</p>
+                              <p>Weather: ${data.weather[0].description}</p>`;
+    })
+}
+
 document.getElementById('weatherForm').addEventListener('submit',showweatherDetails );
+
+document.getElementById('wxByLatLong').addEventListener('submit',weatherByLatLong );
